91cd15571528c5bbadd0d05053964bc951020772
levir <levi.ryza@ums.legacypca.org>
2026-03-25 11:27:24 -0500

    File is made to begin tunning flywheel regressions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelRegressionCode.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subSystems.FlyWheel;
+import org.firstinspires.ftc.teamcode.subSystems.Turret;
+
+import dev.nextftc.control.KineticState;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.impl.ServoEx;
+
+@TeleOp(name = "FlyWheelRegressionCode")
+
+public class FlyWheelRegressionCode extends NextFTCOpMode {
+
+    ServoEx hood = new ServoEx("hood");
+
+    public static Limelight3A limelight = null;
+
+    Turret turret = Turret.getInstance(limelight, telemetry);
+
+    public FlyWheelRegressionCode() {
+        addComponents(
+                new PedroComponent(Constants::createFollower),
+                new SubsystemComponent(FlyWheel.INSTANCE),
+                new SubsystemComponent(turret)
+        );
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        FlyWheel.INSTANCE.isStarted = true;
+    }
+
+    @Override
+    public void onUpdate() {
+
+        turret.relocalizationUpdate();
+
+        if(gamepad1.y){
+            FlyWheel.INSTANCE.FlyWheelControl.setGoal(new KineticState(0, FlyWheel.INSTANCE.FlyWheelControl.getGoal().getVelocity()+100));
+        } else if (gamepad1.b){
+            FlyWheel.INSTANCE.FlyWheelControl.setGoal(new KineticState(0, FlyWheel.INSTANCE.FlyWheelControl.getGoal().getVelocity()+10));
+        } else if (gamepad1.a){
+            FlyWheel.INSTANCE.FlyWheelControl.setGoal(new KineticState(0, FlyWheel.INSTANCE.FlyWheelControl.getGoal().getVelocity()-100));
+        } else if (gamepad1.x){
+            FlyWheel.INSTANCE.FlyWheelControl.setGoal(new KineticState(0, FlyWheel.INSTANCE.FlyWheelControl.getGoal().getVelocity()-10));
+        }
+
+        if(gamepad1.dpad_up){
+            hood.setPosition(hood.getPosition()+.1);
+        } else if(gamepad1.dpad_right){
+            hood.setPosition(hood.getPosition()+.05);
+        } else if(gamepad1.dpad_down){
+            hood.setPosition(hood.getPosition()-.1);
+        } else if(gamepad1.dpad_left){
+            hood.setPosition(hood.getPosition()-.05);
+        }
+
+        if(gamepad1.right_bumper){
+            turret.resetButton();
+        }
+
+        telemetry.addData("Distance from goal: ", turret.distanceOffset);
+        telemetry.addData("Hood Position: ", hood.getPosition());
+        telemetry.addData("leftFlyWheel Speed: ", FlyWheel.INSTANCE.topFW.getVelocity());
+        telemetry.addData("rightFlyWheel Speed: ", FlyWheel.INSTANCE.bottomFW.getVelocity());
+        telemetry.addData("FlyWheel Goal: ", FlyWheel.INSTANCE.FlyWheelControl.getGoal());
+        telemetry.update();
+    }
+
+    @Override
+    public void onInit() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number 0
+    }
+
+    @Override
+    public void onStop() {
+    }
+}
